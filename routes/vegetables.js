410634b0a48dc349a398e2370fe668f9d14f06a0
jayachandraeladandi <112111778+jayachandraeladandi@users.noreply.github.com>
2022-11-26 13:02:29 -0600

    added login

--- routes/vegetables.js
+++ routes/vegetables.js
@@ -10,6 +10,17 @@
 var express = require('express'); 
 const vegetables_controlers= require('../controllers/vegetables'); 
 var router = express.Router(); 
+
+// A little function to check if we have an authorized user and continue on
+
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
  
 /* GET costumes */ 
 router.get('/', vegetables_controlers.vegetables_view_all_Page ); 
@@ -22,7 +33,7 @@ router.get('/detail', vegetables_controlers.vegetables_view_one_Page);
 router.get('/create', vegetables_controlers.vegetables_create_Page);
 
 /* GET create update page */
-router.get('/update', vegetables_controlers.vegetables_update_Page);
+router.get('/update',secured, vegetables_controlers.vegetables_update_Page);
 
 /* GET delete vegetables page */
 router.get('/delete', vegetables_controlers.vegetables_delete_Page);
